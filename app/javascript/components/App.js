90085204a5f63e90a3395fafc4cb86a685a6fbbf
JoshuaMaurer <maurer.joshua@gmail.com>
2023-04-18 15:41:24 -0700

    Initial Commit

---
+++ app/javascript/components/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      App
+    </>
+  )
+}
+
+export default App
