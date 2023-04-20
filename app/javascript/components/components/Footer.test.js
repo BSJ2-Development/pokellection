0fec678352a5e7f690dc9d25217b2bb22c94e056
BxRay95 <bray387@outlook.com>
2023-04-20 09:43:58 -0700

    adds cleanup to home file by creating carouselData file and moving carousel content into it

---
+++ app/javascript/components/components/Footer.test.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import "@testing-library/jest-dom"
+import Footer from "./Footer";
+
+
+const footerRender = () => {
+  render(
+    <BrowserRouter>
+        <Footer />
+      </BrowserRouter>
+  )
+}
+describe("<Footer />", () => {
+  it("renders without crashing", () => {
+    footerRender()
+  })
+
+  it("renders the content in the footer", () => {
+    footerRender()
+    const footerText = screen.getByText(/Pokellection | BSJ2 Development/i)
+    expect(footerText).toBeInTheDocument()
+  })
+})
---
+++ app/javascript/components/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import CarouselData from '../components/CarouselData'
+
+const Home = () => {
+  return (
+    <CarouselData />
+  )
+}
+
+export default Home
