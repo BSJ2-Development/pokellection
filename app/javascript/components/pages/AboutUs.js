f4a70b59fb5c34731ba60351007c225f69907a58
JacobR96 <jrosswalks@gmail.com>
2023-04-25 16:21:18 -0700

    updated styling on about us page and add bio text

--- app/javascript/components/pages/AboutUs.js
+++ app/javascript/components/pages/AboutUs.js
@@ -3,8 +3,13 @@ import { NavLink } from "react-router-dom";
 import Brandon from "../assets/Brandon.jpg"
 import Simon from "../assets/Simon.jpg"
 import Jacob from "../assets/Jacob.jpg"
-
-
+import Josh from "../assets/Josh.jpg"
+import Joshesfav from "../assets/Joshesfav.png"
+import Simonsfav from "../assets/Simonsfav"
+import Brandonsfav from "../assets/Brandonsfav"
+import Jacobsfav from "../assets/Jacobsfav.png"
+import github from "../assets/github.png"
+import linkedin from "../assets/linkedin.png"
 const AboutUs = () => {
   
   // TODO - add pictures and bio info(not all available yet).
@@ -13,57 +18,85 @@ const AboutUs = () => {
       <h1 className='about-us-header' >Learn More About The Dev Team!</h1>
       <div className='about-us-container'>
 
-        <section className='about-me-image-container'>
-          <div className='squares-container-left-side'>
-            <img className='brandon-self-image' src={Brandon} alt='Brandon'/>
-              <a href='https://www.linkedin.com/in/brandon-ray95/' target="_blank"> <img className='linked-in-left' src='https://static.vecteezy.com/system/resources/previews/018/930/587/original/linkedin-logo-linkedin-icon-transparent-free-png.png' alt='brandons linked in logo'></img></a>
-              <div className='squares'>
-                <div className='Brandon-about-me'> 
-                  <p className='brandon-about-me-text'>Hi. I am Brandon.</p>
-              </div>    
-            </div>
-          </div> 
-        </section>
 
-        <section className='about-me-image-container'>
-          <div className='squares-container-right-side'>
-          <img className='simon-self-image' src={Simon} alt='Simon'/>
-            <a href='https://www.linkedin.com/in/sdcurry3/' target="_blank"> <img className='linked-in-right' src='https://static.vecteezy.com/system/resources/previews/018/930/587/original/linkedin-logo-linkedin-icon-transparent-free-png.png' alt='simons linked in logo'></img></a>
-              <div className='squares'>
-                <div className='Simon-about-me'> 
-                  <p> Hi. I am Simon.</p>
-              </div>    
-            </div>
-          </div> 
-        </section>
-
-        <section className='about-me-image-container'>
-          <div className='squares-container-left-side'>
-          <img className='josh-self-image' src={Jacob} alt='josh'/>
-            <a href='https://www.linkedin.com/in/maurer-joshua/' target="_blank"> <img className='linked-in-left' src='https://static.vecteezy.com/system/resources/previews/018/930/587/original/linkedin-logo-linkedin-icon-transparent-free-png.png' alt='joshs linked in logo'></img></a>
-              <div className='squares'>
-                <div className='Josh-about-me'> 
-                  <p> Hi. I am Josh.</p>
-              </div>    
-            </div>
-          </div> 
-        </section>
-
-        <section className='about-me-image-container'>
-          <div className='squares-container-right-side'>
-            <img className='jacob-self-image' src={Jacob} alt='Jacob'/>
-              <a href='https://www.linkedin.com/in/jacob-ross96/' target="_blank"> <img className='linked-in-right' src='https://static.vecteezy.com/system/resources/previews/018/930/587/original/linkedin-logo-linkedin-icon-transparent-free-png.png' alt='jacobs linked in logo'></img></a>
-                <div className='squares'>
-                  <div className='Jacob-about-me'> 
-                    <p> Hi. I am Jacob.</p>
-                </div>    
-            </div>
-          </div> 
-        </section>  
+      <div className='dev-container'>
+        <div className='about-me-image-container'> </div>
+        <div className='about-me-text'>
+        <img className='brandon-self-image' src={Brandon} alt='Brandon'/>
+        <div className='links-to-social'>
+          <a href='https://www.linkedin.com/in/brandon-ray95/' target="_blank"> <img className='linked-in-right' src={linkedin} alt='jacobs linked in logo'></img></a>        
+          <a href='https://github.com/BxRay95' target="_blank"> <img className='linked-in-right' src={github} alt='jacobs linked in logo'></img></a>
+          </div>
+          <p className='brandon-about-me-text'>Hi. I am Brandon.</p>
+          <p className='bio-section' > I was the Tech Lead for Pokellection. I am 27 years old and I enjoy my free time at the lake in my kayaks or playing video games. My favorite pokemon has and always will be Eevee.</p> 
+          <p>Favorite pokemon: Eevee</p>
+          <img className='fav_pokemon' src='https://assets.pokemon.com/assets/cms2/img/pokedex/full/133.png'/>
         </div>
-      <button className='home-page-button'>
-        <NavLink to="/">View Home Page</NavLink>
-      </button>
+      </div>
+
+      <br/>
+
+      <div className='dev-container'>
+        <div className='-me-image-container'> </div>
+        <div className='about-me-text'>
+        <img className='brandon-self-image' src={Simon} alt='Simon'/>
+        <div className='links-to-social'>
+         <a href='https://www.linkedin.com/in/sdcurry3/' target="_blank"> 
+         <img className='linked-in-right' src={linkedin} alt='jacobs linked in logo'></img></a>
+          <a href='https://github.com/SDCurry3' target="_blank"> 
+          <img className='linked-in-right' src={github} alt='jacobs linked in logo'></img></a>
+          </div>
+          <p className='simon-about-me-text'> Hi. I am Simon.</p>
+          <p className='bio-section'> I'm the Project Manager for Pokellection. My favorite Pokemon is Ditto because it's the most versatile, they like people, and it's probably super fun to squish.</p>
+          <p>Favorite pokemon: Ditto.</p>
+          <img className='fav_pokemon' src={Simonsfav}/>
+        </div>
+      </div>
+      
+      <br/>
+
+      <div className='dev-container'>
+        <div className='about-me-image-container'>   </div>
+        <div className='about-me-text'>
+        <img className='brandon-self-image' src={Josh} alt='Josh'/>
+        <div className='links-to-social'>
+          <a href='https://www.linkedin.com/in/maurer-joshua/' target="_blank"> 
+          <img className='linked-in-right' src={linkedin} alt='jacobs linked in logo'></img></a>
+              <a href='https://github.com/JoshuaMaurer' target="_blank"> 
+              <img className='linked-in-right' src={github} alt='jacobs linked in logo'></img></a>
+              </div>
+          <p className='josh-about-me-text'> Hi. I am Josh. </p>
+          <p className='bio-section'> I'm the Product owner for Pokellection. A passionate software developer with a love for travel and exploring the world, sitting down with a good book and cup of coffee, and catching 'em all in the world of Pokémon. Having just finished the bootcamp at LEARN Academy, Josh is constantly honing his coding skills and staying up to date on the latest trends in the tech world. But when he's not glued to his computer screen, you can find him immersed  in new cultures while traveling or getting lost in a page-turning novel. And of course, there is always time for a quick Pokémon battle or two. </p>
+                  <p>Favorite Pokemon: Cubone</p>
+          <img className='fav_pokemon' src={Joshesfav}/>
+        </div>
+      </div>
+  
+      <br/>
+
+      <div className='dev-container'>
+        <div className='about-me-image-container'>
+        </div>
+        <div className='about-me-text'>
+          <img className='brandon-self-image' src={Jacob} alt='Jacob'/>
+          <div className='links-to-social'>
+          <a href='https://www.linkedin.com/in/jacob-ross96/' target="_blank"> 
+          <img className='linked-in-right' src={linkedin} alt='jacobs linked in logo'></img></a>
+          <a href='https://github.com/JacobR96' target="_blank"> <img className='linked-in-right' src={github} alt='jacobs linked in logo'></img></a>
+          </div>
+          <p className='brandon-about-me-text'> Hi. I am Jacob.</p>
+          <p className='bio-section' >I'm the Design Lead for Pokellection. I am a experienced developer with skills in HTML, CSS, Javascript, Node, React, Express.js, APIs, and Git/Github. I am  also certified in technical support, computer networking, and AWS Cloud technologies. Previously I have worked as an Information Management Officer and Help Desk Technician at MSM Technology, LLC. </p> 
+           <p>Favorite Pokemon:Ninetales</p>  
+           <img className='fav_pokemon' src={Jacobsfav}/>
+        </div>
+      </div>
+      
+
+      
+
+     
+    
+        </div>
     </>
   )
 }
